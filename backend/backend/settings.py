693117fb91b305819fcf4d2d40471a0843020772
mioqwe <storozhyk.y@gmail.com>
2024-10-10 23:08:23 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR / 'Media/'
+MEDIA_URL = 'media/'
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
